f8eea3316a80d5f2e95a2a25cc974dda37a855e6
Suraj Ghimire <Suraj.Ghimire@bridgenext.com>
2025-05-19 18:30:00 +0530

    practiced name for auto cd test

--- node-backend/server.js
+++ node-backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors()); // Enable CORS for all origins
 
 app.get('/api/welcome', (req, res) => {
-  res.json({ message: 'Hello from the Node.js backend also!' });
+  res.json({ message: 'Hello from the Node.js backend also by Nepmine !' });
 });
 
 app.listen(PORT, () => {

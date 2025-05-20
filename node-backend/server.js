cb655520ec27f6cf9a6149f2e49b910d2fafb556
Suraj_Ghimire <84459321+Nepmine@users.noreply.github.com>
2025-05-20 15:19:53 +0530

    cd check !

--- node-backend/server.js
+++ node-backend/server.js
@@ -4,10 +4,10 @@ const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors()); // Enable CORS for all origins
+app.use(cors());
 
 app.get('/api/welcome', (req, res) => {
-  res.json({ message: 'Hello from the Node.js backend also by Nepmine !' });
+  res.json({ message: 'Hi from the Node.js backend also by Nepmine !' });
 });
 app.get('/', (req, res) => {
   res.json({ message: 'Welcome to the CICD world !!' });

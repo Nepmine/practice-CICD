03e81382c6351e15e8bbdd3e8ddb8aeb4c3de0ff
Suraj_Ghimire <84459321+Nepmine@users.noreply.github.com>
2025-05-20 15:01:21 +0530

    check cicd

--- node-backend/server.js
+++ node-backend/server.js
@@ -9,6 +9,9 @@ app.use(cors()); // Enable CORS for all origins
 app.get('/api/welcome', (req, res) => {
   res.json({ message: 'Hello from the Node.js backend also by Nepmine !' });
 });
+app.get('\', (req, res) => {
+  res.json({ message: 'Welcome to the CICD world !!' });
+});
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);

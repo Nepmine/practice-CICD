bdc7cb4ca958be4b5a79a0611c7a542a16bdf776
Suraj Ghimire <Suraj.Ghimire@bridgenext.com>
2025-05-20 14:07:58 +0530

    check cd automate by dockerhub webhook

--- node-backend/server.js
+++ node-backend/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors()); // Enable CORS for all origins
 
 app.get('/api/welcome', (req, res) => {
-  res.json({ message: 'Hello from the Node.js backend also by Nepmine !' });
+  res.json({ message: 'Hello from the Node.js backend server by Nepmine !' });
 });
 
 app.listen(PORT, () => {
